f6620e55fd1a12538a3c51c11154cf688cfea279
MD2910 <quangminh29102001@gmail.com>
2023-11-11 20:12:36 +0700

    Mini Hackathon 11

---
+++ minihackathon_lesson11.py
@@ -0,0 +1,27 @@
+#Phần 1, 2 
+Ordinateur = {
+    'HP': 20,
+    'DELL': 50, 
+    'MACBOOK': 12,
+    'ASUS': 30
+}
+
+#Phần 3, 4: 
+Price = {
+    'HP': 600,
+    'DELL': 650, 
+    'MACBOOK': 1200,
+    'ASUS': 400
+}
+
+#Phần 6, 7, 8
+character = {
+    'Name': 'Light',
+    'Age': 17,
+    'Strength': 8,
+    'Defense': 10,
+    'HP': 100,
+    'Backpack': ['Shield', 'Bread Loaf'],
+    'Gold': 100,
+    'Level': 2
+}
